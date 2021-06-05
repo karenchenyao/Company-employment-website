655ae7e41f527c50906ccdf9834a4c9e8ea5ec69
Yao Chen <ychen569@myseneca.ca>
2021-06-05 16:32:51 -0400

    w

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/departments",(req,res)=>{
 });
 
 app.use((req,res)=>{
-    res.status(404).sendFile(path.join(__dirname, "/view/404.html");
+    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 });
 
 data.initialize().then(function(){
