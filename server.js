b50b4c52a40b5738ecb6e18cc8e8b532bf4b09f8
Yao Chen <ychen569@myseneca.ca>
2021-06-05 13:24:51 -0400

    submit1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require ("express");
 var app = express();
 var path = require("path");
+const data = require ("./data-service.js");
+app.use(express.static('public'));
 
 app.use(express.static('public'));
 
@@ -19,4 +21,30 @@ app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname, "/views/about.html"))
 });
 
-app.listen(HTTP_PORT, onHTTPStart);
+app.get("/employees",(req,res){
+    data.getAllEmployees().then(data)=>{
+        res.json(data);
+    }
+});
+
+app.get("/managers",(req,res){
+    data.getManagers().then(data)=>{
+        res.json(data);
+    }
+});
+
+app.get("/departments",(req,res){
+    data.getDepartments().then(data)=>{
+        res.json(data);
+    }
+});
+
+app.use((req,res)=>{
+    res.status(404).send("Pages does not exist");
+});
+
+data.initialize().then(function(){
+    app.listen(HTTP_PORT, onHTTPStart);
+}).catch(function(err){
+    console.log("Unable to start server: " + err);
+});
