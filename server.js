9d1a7bd0abb7269a25fbad4a682962e347ca0eb0
Yao Chen <ychen569@myseneca.ca>
2021-06-20 12:07:42 -0400

    o

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require ("express");
 var app = express();
 var path = require("path");
 var multer = require("multer");
+const fs = require ("fs");
 const data = require ("./data-service.js");
 app.use(express.static('public'));
 
@@ -11,6 +12,16 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHTTPStart(){
     console.log("Express http server listening on PORT: " + HTTP_PORT);
 }
+
+var storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function(req,file,cb){
+        cb(null,Date.now() + path.extname(file.originalname))    
+    }
+
+});
+
+var upload = multer({storage:storage});
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "/views/home.html"))
@@ -50,6 +61,22 @@ app.get("/employees/add",(req,res)=>{
 
 app.get("/images/add",(req,res)=>{
     res.sendFile(path.join(_dirname,"/views/addImages.html"))
+});
+
+app.get("/images", (req,res)=>{
+    var path = "./public/image/uploaded";
+    fs.readdir(path, (err,items)=>{
+        for (var i=0; i<items.length;i++){
+            var images="images" + ':'+ items[i];
+        }
+        res.json(images);
+    })
+
+})
+app.post("/images/add", (req,res) => {
+    upload.single("imageFile").then(() => {
+        res.redirect("/images");
+    });
 });
 
 app.use((req,res)=>{
