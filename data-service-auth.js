d20683e35b5a4c47ab24159ed8197ee602487026
Yao Chen <ychen569@myseneca.ca>
2021-08-04 13:53:30 -0400

    e

---
+++ data-service-auth.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema ({
+    userName:{type:String,unique:true},
+    password:String,
+    email:String,
+    loginHistory:[{
+        dateTime:Date,
+        userAgent:String
+    }]
+});
+
+let User;
+
+module.exports.initialize = function(){
+    return new Promise(function(resolve,reject){
+        let db = mongoose.createConnection("mongodb+srv://YChen:Gilbert02/@SenecaWeb322.rm5hi.mongodb.net/SenecaWeb322?retryWrites=true&w=majority");
+
+        db.on('error',(err)=>{
+            reject(err);
+        });
+        db.once('open',()=>{
+            User=db.model("users",userSchema);
+            resolve();
+        });
+    });
+};
+
+module.exports.registerUser = function(userData) {
+    return new Promise(function(resolve,reject){
+        if(userData.password != userData.password2){
+            reject("Passwords do not match");
+        }else{
+            let newUser = new User(userData);
+            newUser.save((err)=>{
+                if(err){
+                    if(err.code == 11000){
+                        reject("User Name already taken");
+                    }else{
+                        reject("There was an error creating the user: " + err);
+                    }
+                }else{
+                    resolve();
+                };
+            });
+        };
+    });
+};
+
+module.exports.checkUser = function(userData){
+    return new Promise(function(resolve,reject){
+        User.find({userName:userData.userName})
+        .exec()
+        .then((users)=>{
+            if(users.length == 0){
+                reject("Unable to find user: " + userData.userName);
+            }else{
+                bcrypt.compare(userData.password, users[0].password)
+                .then((res)=>{
+                    if (res == true){
+                        users[0].loginHistory.push({dateTime:(new Date()).toString(),userAgent:userData.userAgent});
+                        User.update({userName:user[0].userName},
+                        {$set: {loginHistory: users[0].loginHistory}},
+                        {multi:false})
+                        .exec()
+                        .then(()=>{
+                            resolve(users[0]);
+                        })
+                        .catch((err)=>{
+                            reject("There was an error verifying the user: " + err);
+                        });
+                    }else{
+                        reject("Incorrect Password for user: " + userData.userName);
+                    }
+                });
+            }
+        }).catch((err)=>{
+            reject("Unable to find user: " + userData.userName);
+        })
+    })
+}
--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const fs = require ("fs");
 const data = require ("./data-service.js");
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const dataServiceAuth = require ("./data-service-auth");
+const clientSessions = require("client-sessions");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.engine('.hbs',exphbs({
@@ -46,10 +48,9 @@ app.set('view engine','.hbs');
 
 var HTTP_PORT = process.env.PORT || 8080;
 
-
-function onHTTPStart(){
+/*function onHTTPStart(){
     console.log("Express http server listening on PORT: " + HTTP_PORT);
-}
+}*/
 
 const storage = multer.diskStorage({
     destination: "./public/images/uploaded",
@@ -66,7 +67,26 @@ app.use(function(req,res,next){
     app.locals.activeRoute = (route =="/")?"/":route.replace(/\/$/,"");
     next();
 });
-   
+
+app.use(clientSessions({
+    cookieName: "session",
+    secret: "web322_A6",
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60
+}));
+
+app.use(function(req,res,next){
+    res.locals.session = req.session;
+    next();
+});
+
+function ensureLogin(req,res,next){
+    if(!req.session.user){
+        res.redirect("/login");
+    }else{
+        next();
+    }
+}
 
 app.get("/", function(req,res){
     res.render("home");
@@ -76,22 +96,61 @@ app.get("/about", function(req,res){
     res.render("about")
 });
 
-app.get("/images/add",(req,res)=>{
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.post('/register', (req,res)=>{
+    dataServiceAuth.registerUser(req.body).then(()=>{
+        res.render("register",{successMessage: "User created"});
+    }).catch((err)=>{
+        res.render("register",{errorMessage: err, userName: req.body.userName});
+    });
+});
+
+app.post("/login", (req,res)=>{
+    req.body.userAgent = req.get('User-Agent');
+    dataServiceAuth.checkUser(req.body).then((user)=>{
+        req.session.user = {
+            userName: user.userName,
+            email: user.email,
+            loginHistory: user.loginHistory
+        }
+        res.redirect('/employees');
+    }).catch((err)=>{
+        res.render("login",{errorMessage: err, userName: req.body.userName});
+    });
+});
+
+app.get("/logout", (req,res)=>{
+    req.session.reset();
+    res.redirect('/');
+});
+
+app.get("/userHistory", ensureLogin, (req, res)=>{
+    res.render("userHistory");
+});
+
+app.get("/images/add",ensureLogin,(req,res)=>{
     res.render("addImage");
 });
 
-app.post("/images/add", upload.single("imageFile"), (req,res) => {
+app.post("/images/add", upload.single("imageFile"), ensureLogin, (req,res) => {
         res.redirect("/images");
 });
 
-app.get("/images", (req,res)=>{
+app.get("/images", ensureLogin, (req,res)=>{
     fs.readdir("./public/images/uploaded", (err,items)=>{
         res.render("images",{images:items});
     })
 
 })
 
-app.get("/managers",(req,res)=>{
+app.get("/managers",ensureLogin, (req,res)=>{
     data.getManagers().then((data)=>{
         res.json(data);
     }).catch((err)=>{
@@ -99,7 +158,7 @@ app.get("/managers",(req,res)=>{
     });
 });
 
-app.get("/employees",(req,res)=>{
+app.get("/employees",ensureLogin, (req,res)=>{
         if (req.query.status){
             data.getEmployeesByStatus(req.query.status).then((data)=>{
                 res.render("employees", {employees: data})
@@ -144,7 +203,7 @@ app.get("/employees",(req,res)=>{
         }
 });
 
-app.get("/employee/:empNum",(req,res)=>{
+app.get("/employee/:empNum",ensureLogin, (req,res)=>{
     let viewData = {};
 
     data.getEmployeeByNum(req.params.empNum).then((data)=>{
@@ -175,7 +234,7 @@ app.get("/employee/:empNum",(req,res)=>{
     });
 })
 
-app.get("/employees/add",(req,res)=>{
+app.get("/employees/add", ensureLogin, (req,res)=>{
     data.getDepartments().then((data)=>{
         if (data.length > 0){
             res.render("addEmployee",{departments:data})
@@ -189,19 +248,19 @@ app.get("/employees/add",(req,res)=>{
         })
     });
 
-app.post("/employees/add", (req,res)=>{
+app.post("/employees/add", ensureLogin, (req,res)=>{
         data.addEmployee(req.body).then(()=>{
             res.redirect("/employees");
         })
 });
 
-app.post("/employee/update", (req, res) => {
+app.post("/employee/update", ensureLogin, (req, res) => {
     data.updateEmployee(req.body).then(()=>{
         res.redirect("/employees");
     });
 });
 
-app.get("/employees/delete/:empNum",(req,res)=>{
+app.get("/employees/delete/:empNum", ensureLogin, (req,res)=>{
     data.deleteEmployeeByNum(req.params.empNum).then(()=>{
         res.redirect("/employees")
     }).catch((err)=>{
@@ -209,7 +268,7 @@ app.get("/employees/delete/:empNum",(req,res)=>{
         });
 })
 
-app.get("/departments",(req,res)=>{
+app.get("/departments",ensureLogin, (req,res)=>{
     data.getDepartments().then((data)=>{
         if(data.length >0){
             res.render("departments", {departments: data});
@@ -224,7 +283,7 @@ app.get("/departments",(req,res)=>{
 });
 
 
-app.get("/department/:depId",(req,res)=>{
+app.get("/department/:depId",ensureLogin, (req,res)=>{
     data.getDepartmentById(req.params.depId).then((data)=>{
 
         if (data.length > 0) {
@@ -241,23 +300,23 @@ app.get("/department/:depId",(req,res)=>{
 })
 
 
-app.get("/departments/add",(req,res)=>{
+app.get("/departments/add",ensureLogin, (req,res)=>{
     res.render("addDepartment");
 });
 
-app.post("/departments/add", (req,res)=>{
+app.post("/departments/add", ensureLogin, (req,res)=>{
         data.addDepartment(req.body).then(()=>{
             res.redirect("/departments");
         })
 });
 
-app.post("/department/update", (req, res) => {
+app.post("/department/update", ensureLogin, (req, res) => {
     data.updateDepartment(req.body).then(()=>{
         res.redirect("/departments");
     });
 });
 
-app.get("/departments/delete/:depId",(req,res)=>{
+app.get("/departments/delete/:depId",ensureLogin, (req,res)=>{
     data.deleteDepartmentById(req.params.depId).then(()=>{
         res.redirect("/departments")
     }).catch((err)=>{
@@ -269,8 +328,12 @@ app.use((req,res)=>{
     res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 });
 
-data.initialize().then(function(){
-    app.listen(HTTP_PORT, onHTTPStart);
+dataService.initialize()
+.then(dataServiceAuth.initialize)
+.then(function(){
+    app.listen(HTTP_PORT,function(){
+        console.log("app listening on:" + HTTP_PORT)
+    });
 }).catch(function(err){
     console.log("Unable to start server: " + err);
 });
